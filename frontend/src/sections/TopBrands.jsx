d951f773928b3299cd87d07a9bbf4e8dc9989485
OscarM03 <oscarmutwiri03@gmail.com>
2024-08-19 10:52:14 +0300

    Adding a useEffect feature to help fech the products related to a certain brand from the backend by attaching the brand as a parameter to the url for filtering

--- frontend/src/sections/TopBrands.jsx
+++ frontend/src/sections/TopBrands.jsx
@@ -2,10 +2,41 @@ import { LatestProductsList } from "../constants";
 import { IphoneLogo, SamsungLogo } from "../utils";
 import { useState, useEffect } from "react";
 import TopBrandsCard from "../components/TopBrandsCard";
+import api from "../api";
 
 const TopBrands = () => {
     const [columns, setColumns] = useState(3);
+    const [appleProducts, setAppleProducts] = useState([]);
+    const [samsungProducts, setSamsungProducts] = useState([]);
 
+    useEffect(() => {
+        const fetchAppleProducts = async () => {
+            try {
+                const response = await api.get('api/products/', {
+                    params: {q: 'Apple'}
+                });
+                const data = response.data;
+                setAppleProducts(data)
+
+            } catch (error) {
+            console.error('Error fetching product list', error);
+            }
+        };
+
+        const fetchSamsungProducts = async () => {
+            try {
+                const response = await api.get('api/products/', {
+                    params: { q: 'samsung' }
+                });
+                setSamsungProducts(response.data);
+            } catch (error) {
+                console.error('Error fetching Samsung products:', error);
+            }
+        };
+
+        fetchAppleProducts();
+        fetchSamsungProducts();
+}, []);
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth < 750) {
@@ -52,11 +83,11 @@ const TopBrands = () => {
                 </div>
             </div>
             <div className={`grid ${getGridColumnsClass()} max-xl:gap-4 mx-10 gap-4 max-md:gap-6 mt-6`}>
-                {LatestProductsList.slice(0, columns + columns).map((product) => (
+                {appleProducts.slice(0, columns + columns).map((product) => (
                     <TopBrandsCard
-                    key={product.name}
+                    key={product.id}
                     name={product.name} 
-                    image={product.image}
+                    image={`http://localhost:8000${product.image}`}
                     price={product.price}
                     discount={product.discount}
                     />
@@ -75,11 +106,11 @@ const TopBrands = () => {
                 </div>
             </div>
             <div className={`grid ${getGridColumnsClass()} max-xl:gap-4 mx-10 gap-4 max-md:gap-6 mt-6`}>
-                {LatestProductsList.slice(0, columns + columns).map((product) => (
+                {samsungProducts.slice(0, columns + columns).map((product) => (
                     <TopBrandsCard
-                    key={product.name}
+                    key={product.id}
                     name={product.name} 
-                    image={product.image}
+                    image={`http://localhost:8000${product.image}`}
                     price={product.price}
                     discount={product.discount}
                     />
