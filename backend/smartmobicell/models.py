5a37f1b91ea9fb6d38df188d8a1454fd582ce356
OscarM03 <oscarmutwiri03@gmail.com>
2024-08-21 20:14:59 +0300

    Making sure that other models inherit from the product model since all of them are products

--- backend/smartmobicell/models.py
+++ backend/smartmobicell/models.py
@@ -1,27 +1,34 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 
-
 class Category(models.Model):
     """Categories table"""
     id = ShortUUIDField(primary_key=True, unique=True, length=10, max_length=20, alphabet="abcdef123456")
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
-    
 
-
-class Products(models.Model):
+class Product(models.Model):
     """Products table"""
     id = ShortUUIDField(primary_key=True, unique=True, length=10, max_length=20, alphabet="abcdef123456")
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images/", null=True)
+    Original = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
     discount = models.IntegerField(default=0)
-    type = models.CharField(max_length=100)
+    is_accessory = models.BooleanField(default=False)
     brand = models.CharField(max_length=100)
+    features = models.JSONField(default=dict, blank=True)
+    additional_info = models.TextField(null=True, blank=True)
+    thumbnail_1 = models.ImageField(upload_to="thumbnails/", null=True, blank=True)
+    thumbnail_2 = models.ImageField(upload_to="thumbnails/", null=True, blank=True)
+    thumbnail_3 = models.ImageField(upload_to="thumbnails/", null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -30,29 +37,28 @@ class Products(models.Model):
 
     def __str__(self):
         return self.name
-    
 
-class DisplayProduct(models.Model):
+class DisplayProduct(Product):
     """DisplayProduct table"""
-    id = ShortUUIDField(primary_key=True, unique=True, length=10, max_length=20, alphabet="abcdef123456")
-    name = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="images/", null=True)
-    discount = models.IntegerField(default=0)
-    brand = models.CharField(max_length=100)
+    pass  # Inherits all fields from Product
 
-    def __str__(self):
-        return self.name
-    
-
-class OfferProduct(models.Model):
+class OfferProduct(Product):
     """Special offer products table"""
-    id = ShortUUIDField(primary_key=True, unique=True, length=10, max_length=20, alphabet="abcdef123456")
-    name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    price = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="images/", null=True)
+    period = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+class PickOfTheWeek(Product):
+    """Pick of the week table"""
+    major_info = models.CharField(max_length=100)
+    more_info = models.CharField(max_length=100)
+    added_info = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Pick of the week"
+        verbose_name_plural = "Pick of the week"
+
+class Laptop(Product):
+    """Laptops table"""
+    elements = models.CharField(max_length=100)
+    storage = models.CharField(max_length=100)
+    added_info = models.CharField(max_length=100)
