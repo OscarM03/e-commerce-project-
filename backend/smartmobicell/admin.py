5c0d4f2e0b901cfb14c9c0dff78a0ae9ee3c8ee6
OscarM03 <oscarmutwiri03@gmail.com>
2024-08-21 20:53:58 +0300

    Registering the laptop section

--- backend/smartmobicell/admin.py
+++ backend/smartmobicell/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category, Products, DisplayProduct, OfferProduct
+from .models import Category, Product, DisplayProduct, Laptop, OfferProduct, PickOfTheWeek
 
 admin.site.register(Category)
-admin.site.register(Products)
+admin.site.register(Product)
 admin.site.register(DisplayProduct)
 admin.site.register(OfferProduct)
+admin.site.register(PickOfTheWeek)
+admin.site.register(Laptop)
